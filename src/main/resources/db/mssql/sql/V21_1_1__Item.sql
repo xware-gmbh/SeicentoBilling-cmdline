b88a83eec5248fd63ec3fa2b83891a8e11e2f310
Josef Muri <josef.muri@xwr.ch>
2021-03-09 15:45:25 +0100

    AB#385

---
+++ src/main/resources/db/mssql/sql/V21_1_1__Item.sql
@@ -0,0 +1,14 @@
+/***
+ * Add PriceLevel
+ * 
+ ***/
+
+IF COL_LENGTH('[dbo].[Item]', 'itmPriceLevel') IS NULL
+BEGIN
+    ALTER TABLE [dbo].[Item]
+    ADD 
+	itmPriceLevel [smallint] NOT NULL DEFAULT 0;
+
+END
+
+update [dbo].[Item] set itmPriceLevel = 1 where itmitgid = 2;
